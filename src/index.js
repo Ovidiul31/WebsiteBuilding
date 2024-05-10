c2ce23ef8da2bfdaca6f577b069a0aa5ddf2427d
Ovidiu Brasov <ovidiubrasov@192-168-0-120.rdsnet.ro>
2024-05-10 17:28:22 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import particles from 'particles.js'
+import './js/particles.js'
+import "./js/main.js"
